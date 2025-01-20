97a1b30b4529acf200327c85a1de4b648eadf694
Rushinth John <rushinthjohn@gmail.com>
2025-01-20 18:03:12 -0500

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,27 @@
-provider "aws" {
-    region = var.aws_region
-}
-
-# resource "aws_s3_bucket" "b" {
-#   bucket = "tf-nk-state"
-
-#   tags = {
-#     Name        = var.bucket_name
-#   }
-# }
-# resource "aws_s3_bucket" "tf-nk-state" {
-#   bucket = "tf-nk-state"
-# }
-
-# resource "aws_dynamodb_table" "dynamodblocktable" {
-#   name           = var.dynamodb_name
-#   read_capacity  = 5
-#   write_capacity = 5
-#   hash_key       = "TestTableHashKey"
-
-#   attribute {
-#     name = "TestTableHashKey"
-#     type = "S"
-#   }
-# }
-
-
-resource "aws_instance" "ec-2" {
-  ami           = var.ec2_ami
-  instance_type = var.instance_type
-  key_name = var.ec2_keypair
-  count = var.ec2_count
-  vpc_security_group_ids = ["${aws_security_group.ec2-sg.id}"]
-  subnet_id = element(var.subnets, count.index)
-
-  tags = {
-    Name = "${var.environment}.${var.product}-${count.index+1}"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.52.0"
+    }
   }
 }
 
+terraform {
+  required_version = ">=0.13"
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-09988af04120b3591"
+   instance_type = "t2.micro"
+   key_name = "Ansible"
+   tags = {
+      Name = "Example"
+   }
+
+}

c7a8413946213d9e71bb6a71595f19b580a5397d
Rushinth John <rushinthjohn@gmail.com>
2025-01-20 18:50:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,8 +20,7 @@ resource "aws_instance" "myec2" {
    ami = "ami-09988af04120b3591"
    instance_type = "t2.micro"
    key_name = "jenkins-master"
-   vpc_security_group_ids = ["sg-0ba7e7ed09f28cb8c"]
-   subnet_id = "subnet-0313799b1d8f26c37"
+
    tags = {
       Name = "Example"
    }

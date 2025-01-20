d600d64b8fb638aa2e5d1a440c041c159d0b45c8
Rushinth John <rushinthjohn@gmail.com>
2025-01-20 18:20:38 -0500

    Updated VPC Name in main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,47 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.52.0"
-    }
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Fetching the VPC named rushinth-vpc
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["rushinth-vpc"]
   }
 }
 
-terraform {
-  required_version = ">=0.13"
+data "aws_subnets" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
 }
 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
 
-provider "aws" {
-  region     = "us-east-1"
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"] # Amazon's owner ID
 }
 
-resource "aws_instance" "myec2" {
-   ami = "ami-09988af04120b3591"
-   instance_type = "t2.micro"
-   key_name = "Ansible"
-   tags = {
-      Name = "Example"
-   }
+# Create EC2 instance
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
 
+  # Using the first subnet from the fetched list
+  subnet_id = tolist(data.aws_subnets.selected.ids)[0]
+
+  tags = {
+    Name = "MyInstance"
+  }
 }
